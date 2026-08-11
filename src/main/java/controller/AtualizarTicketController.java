c81ff5b4a677b6df00127abf995afc30c4ae8258
JanerJef <janerjeferson@gmail.com>
2026-08-11 20:30:00 -0300

    feat: Atualizar e Listar Controller

--- src/main/java/controller/AtualizarTicketController.java
+++ src/main/java/controller/AtualizarTicketController.java
@@ -1,4 +1,32 @@
 package controller;
 
-public class AtualizarTicketController {
+import dao.CadastroTicketDao;
+import dao.TicketDao;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.TicketModel;
+
+import java.io.IOException;
+
+@WebServlet("/ticket/listar")
+public class AtualizarTicketController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException{
+
+        TicketModel ticket = new TicketModel();
+
+        ticket.setTitulo(request.getParameter(""));
+        ticket.setStatus(request.getParameter(""));
+        ticket.setTexto(request.getParameter(""));
+
+        TicketDao dao = new TicketDao();
+        if(dao.atualizar(ticket)){
+            response.sendRedirect(request.getContextPath() + "");
+        }else{
+            response.sendRedirect(request.getContextPath() + "");
+        }
+    }
 }
--- src/main/java/controller/ListarTicketController.java
+++ src/main/java/controller/ListarTicketController.java
@@ -1,4 +1,28 @@
 package controller;
 
-public class ListarTicketController {
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.TicketModel;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/ticket/listar")
+public class ListarTicketController extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        TicketModel ticket = new TicketModel();
+        List<TicketModel> lista = new ArrayList<>();
+
+        String json = new Gson().toJson(lista);
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);
+    }
 }
